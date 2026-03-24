7bb3b587b3dc08e61beff841dd3a17e1d0e7c123
Tesfaye <ttadesse627@gmail.com>
2026-03-24 20:21:18 +0300

    Create Initial Project

---
+++ src/test/java/et/gov/osta/jobportal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package et.gov.osta.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
