d3be5e238098ed366666743c194be0d4f0078184
Tesfaye <ttadesse627@gmail.com>
2026-03-28 13:27:43 +0300

    Fix: Fixed the resume and job relationship with Candidate, added the setResume helper method

--- src/main/java/et/gov/osta/jobportal/domain/entities/Candidate.java
+++ src/main/java/et/gov/osta/jobportal/domain/entities/Candidate.java
@@ -4,6 +4,7 @@ package et.gov.osta.jobportal.domain.entities;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -18,13 +19,20 @@ public class Candidate {
     private String firstName;
     private String lastName;
 
-    @OneToOne(mappedBy = "candidate", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "candidates", cascade = CascadeType.ALL, orphanRemoval = true)
     private Resume resume;
 
     @ManyToMany(mappedBy = "candidates")
-    private List<Job> appliedJobs;
+    private List<Job> appliedJobs = new ArrayList<>();
 
     @OneToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private AppUser user;
+
+    public void setResume(Resume resume){
+        this.resume = resume;
+        if (resume != null){
+            resume.setCandidate(this);
+        }
+    }
 }
